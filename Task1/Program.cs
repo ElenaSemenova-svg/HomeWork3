9cd41f28b3bbec738c0ca4740888af116398f954
Elena Semenova <leo110274@yandex.ru>
2023-02-08 22:04:54 +0300

    add HomeWork3 Task1,Task2,Task3

---
+++ Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+//14212 -> нет, 12821 -> да, 23432 -> да
+
+int CheckDidit(int number)
+{
+if (number/10000 == number%10 && (number/1000)%10 == (number%100)/10)
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else 
+    Console.WriteLine("Число не является палиндромом");
+return number;
+}
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        
+        if(int.TryParse(Console.ReadLine(), out result) && result >10000 && result < 99999)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число или не пятизначное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int number = GetNumber("Введите пятизначное число: ");
+CheckDidit(number);
+
+
+// Вариант 2 массив
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// if ( number.Length == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"Число {number} является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Число {number} не является палиндромом");
+//     }
+// }
+// else
+//     {
+//         Console.WriteLine("Вы ввели не пятизначное число");
+//     }
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+float GetNumber(string message)
+{
+    float result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        
+        if(float.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+double GetDistanse(float x1, float y1, float z1, float x2, float y2, float z2)
+{
+double result=Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2)*1.0);
+return result;
+}
+
+float x1 = GetNumber("Введите  координату Х первой точки:");
+float y1 = GetNumber("Введите  координату Y первой точки:");
+float z1 = GetNumber("Введите  координату Z первой точки:");
+
+float x2 = GetNumber("Введите  координату Х второй точки:");
+float y2 = GetNumber("Введите  координату Y второй точки:");
+float z2 = GetNumber("Введите  координату Z второй точки:");
+
+Console.WriteLine($"Расстояние между точками в 3D пр-ве {GetDistanse(x1,y1,z1,x2,y2,z2)}");
